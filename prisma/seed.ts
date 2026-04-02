32c25533ba5026a24f59ff25ce7e3fef819b6c99
Keenan Satriatama <cvnvnasdf24@gmail.com>
2026-04-02 14:45:04 +0700

    init seed

---
+++ prisma/seed.ts
@@ -0,0 +1,123 @@
+import "dotenv/config";
+import { PaymentStatus, Role, PrismaClient } from "../src/generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+const prisma = new PrismaClient({ adapter: pool });
+
+async function main() {
+  // create user
+  const user = await prisma.user.create({
+    data: {
+      email: "budi@mail.com",
+      password: "User123",
+      fullName: "Budi User",
+      birthdate: new Date("2000-01-01"),
+      role: Role.USER,
+    },
+  });
+
+  // create organizer
+  const organizer = await prisma.user.create({
+    data: {
+      email: "budiOrganizer@mail.com",
+      password: "Admin123",
+      fullName: "Budi Organizer",
+      birthdate: new Date("1999-01-01"),
+      role: Role.ORGANIZER,
+    },
+  });
+
+  // event
+  const event = await prisma.event.create({
+    data: {
+      name: "Festival Foo Bar",
+      artist: "Foo Bar",
+      location: "Foo Bar Convention Center",
+      startDate: new Date("2026-06-01T18:00:00"),
+      endDate: new Date("2026-06-01T23:00:00"),
+      totalTicket: 1000,
+      category: "Concert",
+      description: "Ada ada aja festival.",
+      organizerId: organizer.id,
+    },
+  });
+
+  // tickets
+  const ticketVIP = await prisma.ticket.create({
+    data: {
+      ticketLevel: "VIP",
+      availableTicket: 100,
+      eventId: event.id,
+    },
+  });
+
+  const ticketRegular = await prisma.ticket.create({
+    data: {
+      ticketLevel: "Regular",
+      availableTicket: 500,
+      eventId: event.id,
+    },
+  });
+
+  // coupon
+  const coupon = await prisma.coupon.create({
+    data: {
+      amount: 50,
+      expiredDate: new Date("2026-07-01"),
+      userId: user.id,
+    },
+  });
+
+  // voucher
+  const voucher = await prisma.voucher.create({
+    data: {
+      amount: 100,
+      expiredDate: new Date("2026-07-01"),
+      userId: user.id,
+      organizerID: organizer.id,
+    },
+  });
+
+  // transaction
+  const transaction = await prisma.transaction.create({
+    data: {
+      paymentStatus: PaymentStatus.PAID,
+      userId: user.id,
+      ticketId: ticketVIP.id,
+      couponId: coupon.id,
+      voucherId: voucher.id,
+    },
+  });
+
+  // review
+  await prisma.review.create({
+    data: {
+      text: "jorok amet",
+      rating: 2,
+      transactionId: transaction.id,
+      eventId: event.id,
+      reviewerId: user.id,
+    },
+  });
+
+  // points
+  await prisma.point.create({
+    data: {
+      amount: 200,
+      expiredDate: new Date("2026-12-31"),
+      userId: user.id,
+    },
+  });
+
+  console.log("Database seeded successfully!");
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
