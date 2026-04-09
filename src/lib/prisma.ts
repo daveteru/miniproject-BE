e87232effa0103530c90508dfb1d0fe4726a5636
jellyunicorn <47563484+jellyunicorn@users.noreply.github.com>
2026-04-09 15:11:02 +0700

    Revert "hotfix: omit password from user queries"

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,13 +4,7 @@ import { PrismaClient } from "../generated/prisma/client.js";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
-const omitConfig = {
-  user: {
-    password: true,
-  },
-} as const;
-
-const adapter = new PrismaPg({ connectionString }, { schema: "backend" });
-const prisma = new PrismaClient({ adapter, omit: omitConfig });
+const adapter = new PrismaPg({ connectionString }, {schema: "backend"});
+const prisma = new PrismaClient({ adapter });
 
 export { prisma };
